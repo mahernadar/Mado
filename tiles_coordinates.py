27110e489847bdbcc8eff20e9d53bf5c35e1df85
maher <mahernadar@gmail.com>
2023-06-15 15:53:54 +0200

    added code and data to create the map of the supermarket

---
+++ tiles_coordinates.py
@@ -0,0 +1,34 @@
+customers_avatar_tiles_coordinates = [
+    (3, 0),
+    (4, 0),
+    (5, 0),
+    (5, 0),
+    (6, 0),
+    (7, 0),
+    (8, 0),
+    (3, 1),
+    (4, 1),
+    (5, 1),
+    (5, 1),
+    (6, 1),
+    (7, 1),
+    (8, 1),
+    (4, 15),
+    (5, 15),
+    (6, 15),
+    (7, 15),
+    (8, 15),
+    (1, 11),
+]
+
+fruits_avatar_tiles_coordinates = [
+    (0, 4),  # banana
+    (1, 4),  # orange
+    (1, 5),  # strawberry
+]
+
+drinks_avatar_tiles_coordinates = [(3, 13), (4, 9), (5, 9)]
+
+spices_avatar_tiles_coordinates = [(1, 3), (2, 3)]
+
+dairy_avatar_tiles_coordinates = [(0, 3), (0, 5)]
